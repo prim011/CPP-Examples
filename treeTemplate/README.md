8eebbf4c7da435fda9b25eedaa6ae4594cb3d73d
Francesco Fiaschi <prim011@yahoo.com>
2023-08-29 21:30:21 +0100

    including preliminary treeTemplate

---
+++ treeTemplate/README.md
@@ -0,0 +1,37 @@
+# Quick Reference for Template Linked List Class
+
+This quick example code differs from the traditional linked list, explained in other folders, because of the variable structure of the node. Now the user can define the node completely independently at run time. This has been made possible thanks to the template  definitions in C++ 
+
+The two Template classes have now been defined based on the old Linked Class. The migration of those has been pretty straightforward and the flexibility gained is apparent in the `main()` function when the list is utilized with a different node structure as it was originally created. Two considerations are worth mentioning:
+-	The Node destructor
+-	And the Insertion on the list of new elements
+
+### The Node destructor 
+The Node’s destruction includes also all the resources associated with it; including pointers to other nodes in the field `next`. If a single node needs to be removed/deallocated, it is necessary to set `next = NULL;`,  *before* calling the destructor, otherwise the subsequent linked nodes will be deallocated as well, potentially compromising the list itself, if not careful.
+We thought this was a much better design when it comes to removing the entire list (i.e. calling the list destructor), instead of calling the node destructor for each node within the list destructor. In this way, we have taken advantage of the recursion inherited from the linked list as a structured object.
+
+### Node’s Insertion and unordered linked list
+The function design is aiming for simplicity and therefore the insertion always takes place at the end of the linked list. It is an equivalent of `push_back()` in the standard list object. We preferred this approach to the more complete one where the insertion happens *in order*. In that context, a little bit more complexity should have gone into the `insertNew()` function; we saved that work for later. Based on this consideration the linked list has to be considered *unordered*. In the example code, the `main()` function already includes the nodes in an orderly sequencing pattern, so the issue is not transpiring. 
+
+
+There is also a quick example on `main()` function on how to use the `ListClass<>` object, a small exercise on including nodes, searching dedicated nodes and removing them.
+
+
+The code has been written for C++11, but there is no clear dependency and can be compiled on other toolchains, but we have not tested it.
+
+
+
+## Compiling
+Assuming GCC v.11 and cmake already installed
+
+Only for the first time run the following:
+
+`$ cmake . `
+
+Then successive builds like that:
+
+`$ cmake --build . `
+
+The name of the file to compile and the project are identified in the `CMakeLists.txt` file. 
+Output is the executable `myexample` generated after the build.
+
---
+++ treeTemplate/main.cpp
@@ -0,0 +1,401 @@
+#include <iostream>
+#include <string>
+
+#define MAX_LENGTH  60
+
+using namespace std;
+
+/**
+ * @brief Node definition in the list 
+ *
+ * This structure define the Nodes: the contents and the 
+ * pointers to the left and right elements
+ * 
+ * Comparing two different Nodes is done over the field index.
+ * Therefore implicetly the class will have to those operators
+ * overloaded.
+ */
+template <class I, class S>
+class NodeCell {
+public:
+	I 	index;		// an integer index for the node
+	S	jointName; 	// a string of information
+	NodeCell* dx;		// a link to the right element, 
+	NodeCell* sx;		// a link to the left element.
+	// default constructor
+	NodeCell () {index = 0; sx = dx = NULL;}   
+	// initialization contructor 
+	NodeCell (I i, S s):index(i), jointName(s) {dx=sx=NULL;}
+	// copy constractor
+	NodeCell (const NodeCell& n):
+		index(n.index),
+		jointName(n.jointName)
+		{ sx = n.sx; dx = n.dx; }
+	// destructor: it release all resources linked to it! 
+	// set "sx and dx to NULL if you do not want to remove the rest of the nodes
+	~NodeCell() {
+	   	if(sx)           
+      			delete sx;
+      		if(dx)
+      			delete dx;                    
+      
+   	  	index.~I();
+   	  	jointName.~S();
+	}
+	
+	NodeCell& changeIndex (I i) { index = i; return *this; }
+	bool operator< (const NodeCell &n) { return (index < n.index); }
+	bool operator<= (const NodeCell &n) { return (index <= n.index); }
+	bool operator== (const NodeCell &n) { return (index == n.index); }
+	bool operator!= (const NodeCell &n) { return (index != n.index); }
+	bool operator> (const NodeCell &n) { return (index > n.index); }
+	NodeCell& operator= (const NodeCell &n) { 
+		index = n.index; 
+		jointName = n.jointName;
+		sx = n.sx; dx = n.dx;  
+		return *this;			}
+
+	// overload of the << operator for printing an element
+	friend ostream& operator<< (ostream& os, NodeCell<I,S>& t) {	
+		os << "\n";
+		os << "\tindex: " << t.index;
+		os << "\tJoint Name: " << t.jointName;
+		os << "\tleft: " << t.sx;
+		os << "\tright: " << t.dx << endl; 
+		return os; 	
+	}
+};
+		 	
+
+/**
+ * @brief class for the ninary tree data structure 
+ *
+ * This class will have a pointer to the tree, root, and 
+ * the number of levels in the the tree, lvls. This last
+ * field may not be completely necessary, but it can be used 
+ * to control better the tree's trversing.
+ */
+template <class I, class S>
+class TreeClass {
+
+private:
+	NodeCell<I,S>  *root;	// point to the root element
+
+
+ 	void traverse (ostream&, NodeCell<I,S>* );
+ 			// navigate inorderly and print nodes
+	NodeCell<I,S>* minimum(NodeCell<I,S>*);
+			// service funtion to find the minimum in a branch tree
+	NodeCell<I,S>* maximum(NodeCell<I,S>*);			
+			// service funtion to find the maximum in a branch tree
+	void relinkLeft(NodeCell<I,S> &, NodeCell<I,S>* );
+			// service function for relink during deletion
+	void deletenode (NodeCell<I,S>&, NodeCell<I,S>* );
+			// service function to delete a node
+	void searchNode (NodeCell<I,S>&, NodeCell<I,S>*, NodeCell<I,S>* );	
+				
+
+public:
+	// default contructor
+        TreeClass() { root = NULL; }
+        // Constuctor with initialization
+        TreeClass(I setup, S st) {
+        	root = new NodeCell<I,S> (setup, st);
+        	root->sx=root->dx=NULL;
+        }
+        TreeClass(const NodeCell<I,S> &t) {
+        	root = new NodeCell<I,S> (t);
+        	root->sx=root->dx=NULL;
+        }
+        // default destructor
+        ~TreeClass() { delete (root); }
+        
+ 	bool insertNew (NodeCell<I,S>&);		// equivalent of pop the element in 
+ 	bool delNode (NodeCell<I,S>&);			// equivalent of push the element out
+ 	NodeCell<I,S>* searchItem (NodeCell<I,S>&);	// search an item for its index
+
+ 	// overload of the << operator
+ 	friend ostream& operator<< (ostream& os, TreeClass& t) {
+ 		NodeCell<I,S> *p=t.root;
+	
+		os << "\nTREE PRINTOUT - root: " << p << endl;
+		os << "\n=============\n";
+		
+		t.traverse (os, p);
+		return os; 	
+ 	}
+};
+
+template <class I, class S>
+void TreeClass<I,S>::traverse (ostream& os, NodeCell<I,S> *p) {
+	if (p == NULL)
+		return;
+
+	traverse(os, p->sx);
+	os << *p;
+	traverse(os, p->dx);
+} 
+
+/**
+ * @brief Insert one element in the list
+ * This function inserts one node in the list, at the last position.
+ *
+ * @param the node to be included
+ * @return diagnastic if the insertion was succesfull (true)
+ */
+template <class I, class S>
+bool TreeClass<I,S>::insertNew (NodeCell<I,S> &n) {
+	NodeCell<I,S> *t;
+	bool retVal = false;
+
+	cout << "Inserting Node :" << n << endl;
+		
+	if (root == NULL) {
+		root = new NodeCell<I,S> (n);		// use the copy contructor
+		return true;
+	}
+
+	t = new NodeCell<I,S> (n); 			// use the copy contructor 
+	if (t == NULL)
+		return retVal;  
+
+	NodeCell<I,S> *p = root;		
+	NodeCell<I,S> *q;			
+
+	while (p) {	
+		// have we found the right position?
+		if (n <= *p) {
+			q=p;
+			p=p->sx;
+		} else { 
+			q=p;
+			p=p->dx;
+		}
+	}	
+	
+	// add the node as the last element
+	if (n <= *q) 
+		q->sx = t;
+	else 
+		q->dx = t;	
+
+	return true;
+}
+
+bool notFound = true;
+
+template <class I, class S>
+void TreeClass<I,S>::searchNode (NodeCell<I,S>& n, NodeCell<I,S>* p, NodeCell<I,S> *r) {
+
+	if (notFound)
+	{
+		if (p == NULL)
+			return;
+			
+		r = p;
+		searchNode (n, p->sx, r);
+		if (n == *p) {
+			cout << "... found elemet ";
+			notFound = false; 	
+		}
+		if(notFound) {
+			r = p;
+			searchNode (n, p->dx, r);
+		}
+		r = NULL;
+	} else {
+		return;
+	}
+}
+
+
+/**
+ * @brief search of a gine node in the list
+ * This function searches one node in the list,
+ * according to the index value.
+ *
+ * @param the node to be included
+ * @return a node pointer the the element or NULL is not founded
+ */
+template <class I, class S>
+NodeCell<I,S>* TreeClass<I,S>::searchItem (NodeCell<I,S> & n) {
+	NodeCell<I,S> *p = NULL;
+
+	cout << "Searching for node: " << n;
+	
+	notFound = true;
+	searchNode (n, root, p);
+	
+	if (p)
+		cout << "Found elemet " << *p << endl;
+	
+	return p;
+}
+
+/**
+ * @brief search of the predecessor of a given node in the tree
+ 
+ * This function searches the predecessor of a given node in the tree,
+ * according to the index value. Assumption is that nodes belongs to 
+ * the tree
+ *
+ * @param the node to be searched
+ * @return a node pointer the element or NULL is not founded
+ */
+template <class I, class S>
+NodeCell<I,S>* TreeClass<I,S>::minimum(NodeCell<I,S>* p) {
+        NodeCell<I,S>* current = p;
+        if (current == NULL)
+        	return current;
+        	
+        while (current->sx != NULL) {
+            current = current->sx;
+        }
+        return current;
+    }
+
+template <class I, class S>
+NodeCell<I,S>* TreeClass<I,S>::maximum(NodeCell<I,S>* p) {
+        NodeCell<I,S>* current = p;
+        if (current == NULL)
+        	return current;
+
+        while (current->dx != NULL) {
+            current = current->dx;
+        }
+        return current;
+    }
+
+template <class I, class S>
+void TreeClass<I,S>::relinkLeft(NodeCell<I,S> &n, NodeCell<I,S>* p) {
+
+	if ( n != *(p->dx) )		// we are working on the right branch of p
+		return;
+
+        NodeCell<I,S>* t=p->dx;	// pointing to the node to remove
+        cout << "node to be removed: " << *t << endl; 
+        cout << "current node :" << *p << endl;
+        p->dx = p->dx->dx;		// 1st step
+        cout << "1st step done \n";
+        NodeCell<I,S>* temp;
+        temp = minimum(t->dx);          
+        t->dx = NULL;			// 2nd step 
+        if (p->dx != NULL)		// 3th step
+        	p->dx->sx = t->sx;	
+        cout << "step 3. finished \n";
+        cout << "min : " << temp << endl;
+        NodeCell<I,S>* k = maximum(t->sx);
+        cout << "max : " << k << endl;
+        if (k != NULL) 
+        	k->dx = temp;		// 4th step
+        t->sx = NULL;			// 5th step
+        delete (t);			// 6th step 
+}
+
+template <class I, class S>
+void TreeClass<I,S>::deletenode (NodeCell<I,S>& n, NodeCell<I,S> *p) {
+      
+      if (!notFound) {
+             cout << "node found :" << p << endl;      
+        if (n == *(p->sx)) {
+          cout << "... on the sx branch\n";
+          NodeCell<I,S>* t=p->sx;	// pointing to the node to remove
+          p->sx = p->sx->sx;		// 1st step
+          t->sx = NULL;			// 2nd step
+          
+          delete (t);			// 6th step
+          return;
+        } else if (n == *(p->dx)) {
+          cout << "... on the dx branch\n";
+		relinkLeft(n, p);
+        } else {
+	        cout << "pit fall\n";
+        }
+      
+      } else {        
+        if (p == NULL) 
+            return;
+        else if ((p->sx) && (n < *(p->sx)))
+        	deletenode(n,p->sx->sx);
+        else if ((p->dx) && (n > *(p->dx)))
+        	deletenode(n,p->dx->dx);
+        else if ((n == *(p->sx)) || (n == *(p->dx))) {
+        		notFound = false;
+        		deletenode(n,p);
+              }
+        }
+}
+
+/**
+ * @brief Delete one element from the tree
+ *
+ * This function deletes one node in the tree,
+ * according to the index value.
+ *
+ * @param the node to be deleted
+ * @return diagnastic if the deletion was succesfull (true)
+ */
+template <class I, class S>
+bool TreeClass<I,S>::delNode (NodeCell<I,S> &n) {
+	NodeCell<I,S>* p = root;
+	
+	cout << "Deleting Node: " << n;
+	notFound = true;	
+	deletenode (n,p);
+	return true;
+} 
+
+int main() {
+	cout << "Let's start with the Program Exection \n\n";
+
+/////////// Creation of the Binary Tree	
+	TreeClass<int,string> t(500,"firstJoint");
+	
+	cout << "And this is the first class outcome :" << t <<"\n";	
+
+/////////// Insertion of new Nodes - Test Case
+	NodeCell<int,string> n(0, "Joint Name");
+	
+	if ( !(t.insertNew (n.changeIndex(600))) )
+		return (1);   // failed test 
+
+	if ( !(t.insertNew(n.changeIndex(200))) )
+		return (1);   // failed test 
+
+	if ( !(t.insertNew(n.changeIndex(550))) )
+		return (1);   // failed test 
+
+	if ( !(t.insertNew(n.changeIndex(548))) )
+		return (1);   // failed test 
+
+	if ( !(t.insertNew(n.changeIndex(552))) )
+		return (1);   // failed test 
+	
+	if ( !(t.insertNew(n.changeIndex(650))) )
+		return (1);   // failed test 
+	
+	if ( !(t.insertNew(n.changeIndex(652))) )
+		return (1);   // failed test 
+
+	if ( !(t.insertNew(n.changeIndex(648))) )
+		return (1);   // failed test 
+
+//	if ( !(t.insertNew (n.changeIndex(300))) )
+//		return (1);   // failed test 
+		
+	cout << "Added new nodes:" << t << "\n";
+
+/////////// Search and Deletion of old Nodes - Test Case
+//	if ( t.searchItem(n.changeIndex(300)) != NULL ) 
+//		if ( !(t.delNode(n.changeIndex(300))) )
+//				return (1);   // failed test 
+
+//	if ( t.searchItem(n.changeIndex(600)) != NULL ) 
+		if ( !(t.delNode(n.changeIndex(600))) )
+				return (1);   // failed test 
+
+	cout << "Deleted old nodes:" << t << "\n";
+
+/////////// Program Closure - RegTest passed
+	return (0);   // test passed
+}
