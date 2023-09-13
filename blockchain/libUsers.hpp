3da0ac12a893ebc661b8423d699b9c0478a83467
Francesco Fiaschi <prim011@yahoo.com>
2023-09-13 22:46:45 +0100

    first iteration of the blockchain implementation

---
+++ blockchain/libUsers.hpp
@@ -0,0 +1,149 @@
+#include <iostream>
+
+using namespace std;
+
+static int masterSerialNum = 0;
+
+/**
+ * @brief Abstract base class defining access to the Library
+ *
+ * Define the abstract base class identify from which  Students 
+ * and Professors classes will be derived.
+ * This identity class will be the right permission to access the
+ * library
+ */
+class LibAccess {
+public:
+  // virtual methods (to be implemented by derived classes)
+  // Assuming this is all we need to (digitally)
+  // identify Students and Professors, alike
+  virtual string getName () = 0;
+  virtual bool getPermit () = 0;
+  virtual int getSerialN () = 0;
+  virtual size_t getHash () = 0;
+  
+  // printing out method specific for this class
+  friend ostream& operator<< (ostream& os, LibAccess &l)
+  {
+    os << "\tUser name :" << l.getName();
+    os << "\tSerial Number: " << l.getSerialN();
+    os << "\tactive: ";
+    if (l.getPermit()) {
+      os << "Yes" << endl;
+    } else {
+      os << "No" << endl;
+    }
+    os << " Hash : " << l.getHash ();
+    return os;
+  }  
+};
+
+ 
+/**
+ * @brief Student Class
+ * 
+ * This class contains all the information related
+ * to Students. It has been derived from the abstract
+ * class LibAccess, which defines the interface to
+ * the University Library as access premises, 
+ * and from which it will need to implement four 
+ * methods. The class has been extended with a private
+ * hash function.
+ */
+class Student: private LibAccess {
+  string name;
+  int    serialNS;
+  bool   active;
+  size_t cHash;
+
+  // Custom specialization hash 
+  size_t formHash() {
+    size_t h1 = hash<string>{}(name);
+    size_t h2 = hash<int>{}(serialNS);
+    return h1 ^ (h2 << 1); // or use boost::hash_combine
+  }
+ public:
+  Student() { serialNS = 0; cHash = 0; active = false; name="";}
+  Student (string s): name(s), serialNS(++masterSerialNum), active(true) {
+    cHash = formHash();
+  }
+  // implementation of abstract methods  
+  string getName () { return name; }
+  bool getPermit () { return active; }
+  int  getSerialN () { return serialNS; }
+  size_t getHash () { return cHash; }
+
+  void operator= (Student &b) {
+    name = b.name;
+    serialNS = b.serialNS;
+    active = b.active;
+    cHash = b.cHash;
+  }
+  friend ostream& operator<< (ostream &os, Student &S) {
+    os << " Name : " << S.getName () << endl;
+    os << " Serial Number : " << S.getSerialN() << endl;
+    os << " Active member: ";
+    if (S.getPermit())
+      os << "Yes" << endl;
+    else
+      os << "No" << endl;
+
+    os << " Hash : " << S.getHash ();
+    return os;
+  }
+  
+};
+
+/**
+ * @brief Professors Class
+ * 
+ * This class contains all the information related
+ * to Professors. It has been derived from the abstract
+ * class LibAccess, which defines the interface to
+ * the University Library as access premises, and 
+ * from which it will need to implement four methods.
+ * The class has been extended with a dedicated Hash 
+ * function
+ */
+class Prof: private LibAccess {
+  string name;
+  int    serialNP;
+  bool   active;
+  size_t cHash;
+
+  // Custom specialization hash 
+  size_t formHash() {
+    size_t h1 = hash<string>{}(name);
+    size_t h2 = hash<int>{}(serialNP);
+    return h1 ^ (h2 << 1); // or use boost::hash_combine
+  }
+public:
+  Prof () { serialNP = 0; active = false; cHash = 0; name="";}
+  Prof (string s): name(s), serialNP(++masterSerialNum), active(true) {
+    cHash = formHash();
+  }
+  // implementation of abstract methods
+  string getName() { return name; }   
+  bool getPermit () { return active; }
+  int  getSerialN () { return serialNP; }  
+  size_t getHash () { return cHash; }
+
+  void operator= (Prof &b) {
+    name = b.name;
+    serialNP = b.serialNP;
+    active = b.active;
+  }
+
+  friend ostream& operator<< (ostream &os, Prof &P) {
+    os << " Name : " << P.getName () << endl;
+    os << " Serial Number : " << P.getSerialN() << endl; 
+    os << " Active member: ";
+    if (P.getPermit())
+      os << "Yes" << endl;
+    else
+      os << "No" << endl;
+
+    os << " Hash : " << P.getHash();
+    return os;
+  }
+};
---
+++ blockchain/listTemp.hpp
@@ -0,0 +1,196 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/**
+ * @brief Node definition in the list 
+ *
+ * This structure define the Nodes: the contents and the 
+ * pointer to the next element
+ */
+template <class I, class S>
+class HashCell {
+public:
+	I 	index;		// an integer index for the node
+	S	hashValue; 	// a string of information
+	HashCell* next;		// a link to the next element, 
+				// it can be NULL for a single element
+	// default constructor
+	HashCell () {index = 0; next = NULL;}   
+	// initialization contructor 
+	HashCell (I i, S s):index(i), hashValue(s) {next=NULL;}
+	// copy constractor
+	HashCell (const HashCell& n):index(n.index),hashValue(n.hashValue),next(n.next) {}
+	// destructor: it release all resources linked to it! 
+	// set "next to NULL if you do not want to remove the rest of the nodes
+	~HashCell() {
+	   	if(next)           
+      			delete next;                    
+      
+   	  	hashValue.~S();
+	}
+	
+	HashCell& changeIndex (I i) { index = i; return *this; }
+	bool operator< (const HashCell &n) { return (index < n.index); }
+	bool operator<= (const HashCell &n) { return (index <= n.index); }
+	bool operator== (const HashCell &n) { return (index == n.index); }
+	bool operator!= (const HashCell &n) { return (index != n.index); }
+	bool operator> (const HashCell &n) { return (index > n.index); }
+
+	// overload of the << operator for printing an element
+	friend ostream& operator<< (ostream& os, const HashCell<I,S>& t) {	
+		os << "\n";
+		os << "\t timestamp: " << t.index;
+		os << "\t tree hash value: " << t.hashValue;
+		if (t.next)
+		 os << "\tnext ->: " << t.next->index << endl;
+		return os; 	
+	}
+};
+
+/**
+ * @brief class for the List 
+ */
+template <class I, class S>
+class ListClass {
+
+private:
+	int nodeNum;		// Number of nodes in the list
+	HashCell<I,S>  *head;	// point to the first element
+public:
+	// default contructor
+        ListClass() { nodeNum = 0; head = NULL; }
+        // Constuctor with initialization
+        ListClass(I setup, S st) {
+        	head = new HashCell<I,S> (setup, st);
+        	nodeNum = 1;
+        }
+        ListClass(const HashCell<I,S> &t) {
+        	head = new HashCell<I,S> (t);
+        	nodeNum = 1;
+        }
+        // default destructor
+        ~ListClass() {nodeNum = 0; delete (head); }
+        
+ 	bool insertNew (HashCell<I,S>&);		// equivalent of pop the element in 
+ 	bool delNode (HashCell<I,S>&);		// equivalent of push the element out
+ 	HashCell<I,S>* searchItem (HashCell<I,S>&);	// search an item for its index
+ 	
+ 	// overload of the << operator
+ 	friend ostream& operator<< (ostream& os, const ListClass& t) {
+ 		const HashCell<I,S> *p=t.head;
+		int maxElem =t.nodeNum; 
+	
+		os << "\nHistory - # events:" << maxElem;
+		os << "\n=============\n";
+
+		for (int i=1; i<=maxElem; i++, p=p->next) {
+			os << *p;
+		}	
+		return os; 	
+ 	}
+};
+
+
+/**
+ * @brief Insert one element in the list
+ * This function inserts one node in the list, at the last position.
+ *
+ * @param the node to be included
+ * @return diagnastic if the insertion was succesfull (true)
+ */
+template <class I, class S>
+bool ListClass<I,S>::insertNew (HashCell<I,S> &n) {
+	HashCell<I,S> *t;
+	bool retVal = false;
+		
+	if (head == NULL) {
+		head = new HashCell<I,S> (n);		// use the copy contructor
+		nodeNum=1;
+		return true;
+	}
+
+	t = new HashCell<I,S> (n); 			// use the copy contructor 
+	if (t == NULL)
+		return retVal;  
+
+	HashCell<I,S> *p = head;		
+	while (p->next) {
+		p = p->next;
+	}	
+	
+	// add the node as the last element
+	p->next = t;
+	t->next = NULL; // just making sure!!
+	nodeNum++;
+	retVal = true;			
+
+	return retVal;
+}
+
+/**
+ * @brief search of a gine node in the list
+ * This function searches one node in the list,
+ * according to the index value.
+ *
+ * @param the node to be searched
+ * @return a node pointer the the element or NULL is not founded
+ */
+template <class I, class S>
+HashCell<I,S>* ListClass<I,S>::searchItem (HashCell<I,S> & n) {
+	HashCell<I,S> *p = head;
+	
+	// use overload of operator != for HashCell
+	while (p->next && n != *p) {
+		p = p->next;
+	}
+	
+	if (n == *p)
+		return p;
+	return NULL;
+}
+
+/**
+ * @brief Delete one element from the list
+ * This function deletes one node in the list,
+ * according to the index value.
+ *
+ * @param the node to be deleted
+ * @return diagnastic if the deletion was succesfull (true)
+ */
+template <class I, class S>
+bool ListClass<I,S>::delNode (HashCell<I,S> &n) {
+	HashCell<I,S> *c;
+	bool retVal = false;
+	
+	if (head == NULL)
+		return retVal;
+	
+	if (n == *head) {
+		c = head; 
+		head = head->next;
+		c->next=NULL;	// without this instruction the 
+				// following delete will remove the 
+				// rest of the list
+		delete (c);
+		nodeNum--;
+		return true;
+	}
+		
+	HashCell<I,S> *p = head;
+	while ( p->next->next && (n != *(p->next)) ) {
+		p = p->next;
+	}
+	
+	if  (n == *(p->next))  {
+		c = p->next; 
+		p->next = p->next->next;
+		c->next=NULL;
+		delete (c);
+		nodeNum--;
+		retVal = true;	
+	}	
+	return retVal;
+} 
+
